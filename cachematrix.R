006fc837d6740855788954b6aeda6475e6ab5fdf
marysv <marilena_manole@outlook.com>
2015-03-22 14:32:53 +0000

    added the functions for caching the matrix inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+### Below are two functions that are used to create a special object that stores a numeric matrix and 
+### caches its inverse.
 
-## Write a short comment describing this function
 
+### The 'makeCacheMatrix' function creates a special "matrix" that contains the original matrix value 
+### and its inverse, accessible by these methods: 
+### 1. set the value of the matrix
+### 2. get the value of the matrix
+### 3. set the value of the inverse
+### 4. get the value of the inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        
+        setInverse <- function(i) inv <<- i
+        getInverse <- function() inv
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+### The 'cacheSolve' function calculates the inverse of the special "matrix" created with the above function. 
+### It first checks to see if the inverse has already been calculated. If so, it gets the inverse from 
+### the cache and skips the computation. Otherwise, it calculates the inverse of the data and sets the 
+### value of the inverse in the cache via the setInverse function.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        invMatr <- x$getInverse()
+        if(!is.null(invMatr)) {
+                return(invMatr)             ### getting cached data
+        }
+        
+        data <- x$get()                     ### calculating the inverse
+        invMatr <- solve(data)
+        x$setInverse(invMatr)               ### caching the inverse
+        invMatr                             ### geting the inverse
 }
